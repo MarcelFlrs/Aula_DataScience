b76741f46889bb4240d94785ae3f3f4ae450f3a0
MarcelFlrs <127535882+MarcelFlrs@users.noreply.github.com>
2024-08-29 11:13:39 -0300

    Update testesFuncoesEstatisticas.py

--- Aula5/testesFuncoesEstatisticas.py
+++ Aula5/testesFuncoesEstatisticas.py
@@ -24,3 +24,25 @@ axes[1].step(n, X.cdf(n))
 axes[2].hist(X.rvs(size=1000))
 
 show()
+
+
+
+y = stats.norm()
+
+x = linspace(-5,5,100)
+fig, axes = subplots(3,1, sharex=True)
+
+axes[0].step(n,x.pmf(n))
+axes[1].step(n, x.cdf(n))
+axes[2].hist(y.rvs(size=1000), bins=50)
+
+show()
+
+
+x.mean(), x.std(), x.var()
+
+y.mean(), y.std(), y.var()
+
+t_statistc, p_value = stats.ttest_ind(x.rvs(size=1000), y.rvs(size=1000))
+print("t_statistc = "), t_statistc
+print("p_value"), p_value
